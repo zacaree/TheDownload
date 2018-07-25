cbf113a14a9db1f8ecefb035916b61cf2aa320dd
Zac <zacaree@gmail.com>
2018-07-25 15:31:08 -0500

    A beautiful refactoring of videoHandler.js. forEach for the win

--- src/scripts/videoHandler.js
+++ src/scripts/videoHandler.js
@@ -1,36 +1,21 @@
 
 export default function videoHandler() {
-  const tigerVideo = document.getElementById('tigerVideo');
-  const tigerVideoCover = document.getElementById('tigerVideoCover');
-  const powerProVideo = document.getElementById('powerProVideo');
-  const powerProVideoCover = document.getElementById('powerProVideoCover');
+  const videos = Array.from(document.querySelectorAll('.video'));
 
-  // This is an IE fix.
-  // Resets the video's "autoplay" attribute when page reloads. 
-  // Keeps video from playing automatically.
-  document.addEventListener("DOMContentLoaded", function(e) {
-    tigerVideo.src = tigerVideo.src.replace("&autoplay=1", "");
-    powerProVideo.src = powerProVideo.src.replace("&autoplay=1", "");
-  });
+  videos.forEach(video => {
+    document.addEventListener('DOMContentLoaded', resetAutoplayValue, false);
+    function resetAutoplayValue() {
+      video.src = video.src.replace("&autoplay=1", "");
+    }
 
-  tigerVideoCover.addEventListener('click', playTigerVideo, false)
-  powerProVideoCover.addEventListener('click', playPowerProVideo, false)
+    const videoCover = video.nextElementSibling;
+    videoCover.addEventListener('click', playVideo, false);
 
-  function playTigerVideo() {
-    tigerVideo.src += "&autoplay=1";
-    tigerVideoCover.style.opacity = 0;
-    tigerVideoCover.style.transform = 'scale(1.1)';
-    setTimeout(function() {
-      tigerVideoCover.style.display = 'none';
-    }, 500)
-  }
-
-  function playPowerProVideo() {
-    powerProVideo.src += "&autoplay=1";
-    powerProVideoCover.style.opacity = 0;
-    powerProVideoCover.style.transform = 'scale(1.1)';
-    setTimeout(function() {
-      powerProVideoCover.style.display = 'none';
-    }, 500)
-  }
+    function playVideo() {
+      video.src += "&autoplay=1";
+      videoCover.style.opacity = 0;
+      videoCover.style.transform = 'scale(1.1)';
+      setTimeout(() => videoCover.style.display = 'none', 500);
+    }
+  })
 }
